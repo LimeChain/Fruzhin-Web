2909b29f9ce0450b2551fa769115fe9131c5beba
Yordan Atanasov <99724706+Zurcusa@users.noreply.github.com>
2024-08-14 15:57:19 +0300

    feat: add functionality to interact with localStorage. (#5)
    * feat: add functionality to interact with localStorage.
    
    * refactor: remove deprecated LocalStorage bean.
    
    * refactor: address pr comments

--- src/main/java/com/limechain/utils/json/ObjectMapper.java
+++ src/main/java/com/limechain/utils/json/ObjectMapper.java
@@ -4,6 +4,8 @@ import com.limechain.utils.DivLogger;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Base64;
 import java.util.List;
 import java.util.Map;
@@ -66,9 +68,18 @@ public class ObjectMapper {
         if (type.isInstance(value)) {
             return (T) value;
         } else if (type == Integer.class || type == int.class) {
-            return (T) (Integer) ((Number) value).intValue();
+            return handleWholeNumber(value, (long) Integer.MIN_VALUE, (long) Integer.MIN_VALUE);
+        } else if (type == Long.class || type == long.class) {
+            return handleWholeNumber(value, Long.MIN_VALUE, Long.MAX_VALUE);
         } else if (type == Double.class || type == double.class) {
-            return (T) (Double) ((Number) value).doubleValue();
+            BigDecimal bigDecimalValue = new BigDecimal((String) value);
+            double doubleValue = bigDecimalValue.doubleValue();
+            if (doubleValue == Double.POSITIVE_INFINITY || doubleValue == Double.NEGATIVE_INFINITY) {
+                throw new ArithmeticException("Value out of range for Double: " + value);
+            }
+            return (T) Double.valueOf(doubleValue);
+        } else if (type == BigInteger.class) {
+            return (T) new BigInteger((String) value);
         } else if (type == Boolean.class || type == boolean.class) {
             return (T) value;
         } else if (type == String.class) {
@@ -86,6 +97,16 @@ public class ObjectMapper {
         throw new RuntimeException("Unsupported field type: " + type);
     }
 
+    @SuppressWarnings("unchecked")
+    private <T> T handleWholeNumber(Object value, Long min, Long max) {
+        BigInteger bigIntValue = new BigInteger((String) value);
+        if (bigIntValue.compareTo(BigInteger.valueOf(min)) < 0 ||
+            bigIntValue.compareTo(BigInteger.valueOf(max)) > 0) {
+            throw new ArithmeticException("Value out of range number type: " + value);
+        }
+        return (T) Integer.valueOf(bigIntValue.intValue());
+    }
+
     private Object convertArray(Class<?> componentType, List<?> jsonArray) {
         Object array = Array.newInstance(componentType, jsonArray.size());
         for (int i = 0; i < jsonArray.size(); i++) {
@@ -100,6 +121,7 @@ public class ObjectMapper {
             clazz == Integer.class || clazz == Long.class ||
             clazz == Double.class || clazz == Float.class ||
             clazz == Boolean.class || clazz == Byte.class ||
-            clazz == Short.class || clazz == Character.class;
+            clazz == Short.class || clazz == Character.class ||
+            clazz == BigInteger.class;
     }
 }
