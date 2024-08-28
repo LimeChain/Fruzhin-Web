5918e0fae362412fcfa5303c7cd7987ebe4ddc1b
Мurаd Hаmzа <murad.hamza@limechain.tech>
2024-08-28 12:12:53 +0300

    chore: remove stream object

--- src/main/java/com/limechain/network/StrictProtocolBinding.java
+++ src/main/java/com/limechain/network/StrictProtocolBinding.java
@@ -1,11 +1,5 @@
 package com.limechain.network;
 
-import com.limechain.network.wrapper.Stream;
-import org.teavm.jso.JSBody;
-import org.teavm.jso.core.JSPromise;
-
-import java.util.concurrent.atomic.AtomicReference;
-
 public abstract class StrictProtocolBinding {
     String protocolId;
 
@@ -13,34 +7,4 @@ public abstract class StrictProtocolBinding {
         this.protocolId = protocolId;
     }
 
-    public Stream dialPeer(/*PeerId peer*/) {
-        Object peer1 = getPeer();
-        JSPromise<Object> dial = dial(peer1, protocolId);
-        final var lock = new Object();
-        AtomicReference<Stream> stream = new AtomicReference<>();
-
-        dial.then((result) -> {
-            stream.set((Stream) result);
-            synchronized (lock) {
-                lock.notify();
-            }
-            return null;
-        });
-
-        synchronized (lock) {
-            try {
-                lock.wait();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        return stream.get();
-    }
-
-    @JSBody(params = {"peerId", "protocolId"}, script = "return (async () => ItPbStream.pbStream(await libp.dialProtocol(peerId, protocolId)))()")
-    private static native JSPromise<Object> dial(Object peerId, String protocolId);
-
-    @JSBody(script = "return libp.getConnections()[0].remotePeer;")
-    private static native Object getPeer();
 }
--- src/main/java/com/limechain/network/protocol/warp/WarpSync.java
+++ src/main/java/com/limechain/network/protocol/warp/WarpSync.java
@@ -1,24 +1,15 @@
 package com.limechain.network.protocol.warp;
 
-import com.limechain.exception.global.ExecutionFailedException;
-import com.limechain.exception.global.ThreadInterruptedException;
 import com.limechain.network.StrictProtocolBinding;
-import com.limechain.network.kad.dto.Host;
-import com.limechain.network.kad.dto.PeerId;
-import com.limechain.network.protocol.warp.dto.WarpSyncRequest;
 import com.limechain.network.protocol.warp.dto.WarpSyncResponse;
-import com.limechain.network.wrapper.Stream;
 import lombok.extern.java.Log;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 
 @Log
 public class WarpSync extends StrictProtocolBinding {
 
-    private String protocolId;
+    private final String protocolId;
 
     public WarpSync(String protocolId) {
         super(protocolId);
