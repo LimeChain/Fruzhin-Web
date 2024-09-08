a2acda14ea59762cf43863daf1831f15e1c448fd
Georgi Grigorov <georgi.grigorov@limechain.tech>
2024-09-08 20:05:08 +0300

    Initial try which was not successful

---
+++ src/main/webapp/js/lib/external.js
@@ -0,0 +1,11 @@
+import 'https://unpkg.com/@chainsafe/libp2p-yamux/dist/index.min.js';
+import 'https://unpkg.com/@chainsafe/libp2p-noise/dist/index.min.js';
+import 'https://unpkg.com/@libp2p/websockets/dist/index.min.js';
+import 'https://unpkg.com/@libp2p/kad-dht/dist/index.min.js';
+import 'https://unpkg.com/@libp2p/identify/dist/index.min.js';
+import 'https://unpkg.com/@libp2p/bootstrap/dist/index.min.js';
+import 'https://unpkg.com/@libp2p/ping/dist/index.min.js';
+import 'https://unpkg.com/@chainsafe/libp2p-gossipsub/dist/index.min.js';
+import 'https://unpkg.com/libp2p/dist/index.min.js';
+import 'https://unpkg.com/it-pipe/dist/index.min.js';
+import 'https://unpkg.com/it-pb-stream/dist/index.min.js';
---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,5 @@
+import './blake2b.js';
+import './ed25519.js';
+import './http.js';
+
+import './lib/external.js';
