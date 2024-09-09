702f0bad73e22977e41cbfd6b5a87ccf236f348b
Georgi Grigorov <georgi.grigorov@limechain.tech>
2024-09-09 10:12:07 +0300

    Second unsuccessful try

---
+++ src/main/webapp/js/fruzhin.js
@@ -0,0 +1,5 @@
+import './lib/external.js';
+
+import './blake2b.js';
+import './ed25519.js';
+import './http.js';
--- src/main/webapp/js/main.js
+++
@@ -1,5 +0,0 @@
-import './blake2b.js';
-import './ed25519.js';
-import './http.js';
-
-import './lib/external.js';
